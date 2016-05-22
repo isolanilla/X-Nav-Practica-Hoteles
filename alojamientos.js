9b4597f22525fe50a891089251f81e6396d57ebd
Ismael Solanilla <i.solanilla3@gmail.com>
2016-05-22 13:41:11 +0200

    finalizada

--- alojamientos.js
+++ alojamientos.js
@@ -9,28 +9,125 @@
 //
 var markers = []
 var colecciones = {}
+var google = {}
+var dgeneral = {}
 var coleccion_selecionada = undefined
+var apiKey = 'AIzaSyD3ufxXtcSydhozFQgxeLFMw-eYToi8bIc ';
+var HotelSelecionado = undefined
+function actulizar_google() {
+  html_google = "<p>Hotel selecionado " + accomodations[HotelSelecionado].basicData.name + "</p> <ul>"
+  var arr = google[HotelSelecionado]
+  for(var foo in google ){
+    if(HotelSelecionado==foo){
+      console.log(foo)
+      var arr = google [foo]
+      for (var i = 0 ;i<arr.length;i++) {
+        html_google += "<li id='"+arr[i].name+"'><img class='imgGoogle' src='"+ arr[i].img +"'>" + arr[i].name + "</li>"
+      }
+      
+    }
+  }
+  html_google+="</ul>"
+  $("#google").html(html_google)
+}
 
-function cargar_info(no){
+  //html += "<li id='"+resp.displayName+"'>" + resp.displayName + "</li>"
 
+function crearIdGoogle(id_google){
+    gapi.client.setApiKey(apiKey);
+    name=id_google  
+    gapi.client.load('plus', 'v1', function() {
+        var request = gapi.client.plus.people.get({
+          'userId': name
+        });
+        request.execute(function(resp) {
+          //image.src = resp.image.url;
+          var object = {name:resp.displayName,img:resp.image.url}
+          var arr = []
+          arr = google[HotelSelecionado]
+          if(arr == undefined){
+            arr = []
+          }
+          arr.push(object)          
+          google[HotelSelecionado] = arr
+          actulizar_google()
+        });
+      });
+}
+
+function sendGoogle() {
+  id_google = $("#idGoogle").val()
+  var arr = []
+    for(no in google){
+      if(no == HotelSelecionado){
+        arr = google[no]
+        for (var i = 0; i < google[no].length; i++) {
+          if(id_google == arr[i]){
+            alert("id ya almacenado")
+            return
+          }else{
+            crearIdGoogle(id_google)
+            /*arr.push(id_google)
+            google[no]=arr
+            actulizar_google()*/
+            return
+          }
+        }
+      }
+    }
+    crearIdGoogle(id_google)
+}
+
+
+function info_alojados(no) {
   var accomodation = accomodations[no];
-  var lat = accomodation.geoData.latitude;
-  var lon = accomodation.geoData.longitude;
-  var url = accomodation.basicData.web;
   var name = accomodation.basicData.name;
   var desc = accomodation.basicData.body;
   var cat = accomodation.extradata.categorias.categoria.item[1]['#text'];
   var html=""
-  var markerexists= false;
+
   if (accomodation.extradata.categorias.categoria.subcategorias != undefined){
       var subcat = accomodation.extradata.categorias.categoria.subcategorias.subcategoria.item[1]['#text'];
   }else{
     subcat=""
   }
 
-  html+= '<div class="infohotel"> <div class="row"> <div class="col-md-6 col-xs-12"><h2>' + name + '</h2>';
+  html+= '<h2 id="'+no+'">' + name + '</h2>';
+  html+= '<p>Type: ' + cat + ', subtype: ' + subcat + '</p>' + desc ;
+  $("#infohotel").html(html);
+  actulizar_google()
+
+}
+
+
+
+function info(no) {
+  var accomodation = accomodations[no];
+  var name = accomodation.basicData.name;
+  var desc = accomodation.basicData.body;
+  var cat = accomodation.extradata.categorias.categoria.item[1]['#text'];
+  var html=""
+
+  if (accomodation.extradata.categorias.categoria.subcategorias != undefined){
+      var subcat = accomodation.extradata.categorias.categoria.subcategorias.subcategoria.item[1]['#text'];
+  }else{
+    subcat=""
+  }
+
+  html+= '<div class="infohotel"> <div class="row"> <div class="col-md-6 col-xs-12"><h2 id="'+no+'">' + name + '</h2>';
   html+= '<p>Type: ' + cat + ', subtype: ' + subcat + '</p>' + desc ;
   html+=  '</div> <div class="col-md-6 col-xs-12">';
+
+  return html;
+}
+
+
+function cargar_info(no){
+  var accomodation = accomodations[no];
+  var html=""
+
+  info_alojados(no)
+  html=info(no)
   if (accomodation.multimedia.media != undefined) {
     if(accomodation.multimedia.media.length != undefined){
       html+= '<div id="myCarousel" class="carousel slide" data-ride="carousel"> <ol class="carousel-indicators">'
@@ -55,7 +152,7 @@ function cargar_info(no){
       html+= '<span class="sr-only">Next</span></a>'
       }
     }else{
-      html+= '<img src="/images/not-found.jpg">';
+      html+= '<img src="images/not-found.jpg">';
     }
   }else{
     html+= '<img src="/images/not-found.jpg">';
@@ -66,44 +163,67 @@ function cargar_info(no){
 }
 
 function load(){
-
-  console.log("han pulsado load");
   $("#formLoad").show()
-
 }
 
 function load_git(){
-
-    console.log("han pulsado load git");
-
+    colecciones = {}
+    google={}
+    $.getJSON($("#url").val(), function(data) {
+      for(name in data){
+        console.log(data)
+        console.log(name)
+          if(name="colecciones"){
+            for(nameC in data[name]){
+              newData = data[name]
+              colecciones[nameC] = newData[nameC]
+            }
+          }
+          if (name="google"){
+            console.log("rellenamoss google")
+            for(nameG in data[name]){
+              newData = data[name]
+              google[nameG] = newData[nameG]
+            }
+          }
+      }
+      $("#droppable").show()
+      MostrarColecciones()
+    })
+    $("#formLoad").hide()
 }
 
 
 function save_git(){
-
   var token = $("#token").val()
   var repo = $("#repo").val()
   var usuario = $("#usuario").val()
-  console.log(token + repo + usuario);
+  var nombreFichero = $("#fich").val()
+  dgeneral["colecciones"] = colecciones
+  dgeneral["google"] = google
+  var github =  new Github({token:token,auth: "oauth"});
+  var repo = github.getRepo(usuario, repo);
+  repo.write('master', nombreFichero, JSON.stringify(dgeneral), 'colecciones', function(err) {});
+  $("#formSave").hide()
+
+
+
 }
 
 function save(){
-  console.log("han pulsado save");
   $("#formSave").show()
 }
 
 function actualizar_list3(){
-    console.log("actualizamos list")
-    console.log(coleccion_selecionada)
-    console.log(colecciones[coleccion_selecionada]);
 
-    var html = "<ul>"
+    var html = "<p>Colección selecionada " + coleccion_selecionada + "</p> <ul>"
     var arr = colecciones[coleccion_selecionada]
     for(var i =0;i<arr.length;i++){
       html += "<li id='"+arr[i].id+"'>" + arr[i].name+ "</li>"
     }
     html+="</ul>"
     $("#droppable").html(html)
+    $("#listSelCol").html($("#droppable").html())
 }
 
 function MostrarColecciones(){
@@ -120,10 +240,9 @@ function MostrarColecciones(){
 function show_hotel(){
   id = $(this).attr('id');
   coleccion_selecionada = id;
+  $("#listSelCol").css("display", "");
+  $("#droppable").html("<p>Colección selecionada " + coleccion_selecionada + "</p>")
   actualizar_list3()
-  console.log("pinchan en una coleccion para mostrar los hoteles" + id)
-
-
 }
 
 function submitColeccion(){
@@ -136,7 +255,9 @@ function submitColeccion(){
     }
   }
   var hoteles = [];
+  var idGoogle = []
   colecciones[nombre] = hoteles;
+
 
   $("#list3").show();
   $("#droppable").show();
@@ -147,19 +268,19 @@ function submitColeccion(){
 function del(lat,lon){
 
   for (var i = 0; i < markers.length; i++) {
+      if (markers[i]._latlng.lat==lat && markers[i]._latlng.lng==lon){
 
-            if (markers[i]._latlng.lat==lat && markers[i]._latlng.lng==lon){
-
-                map.removeLayer(markers[i]);
-                markers.splice(i, 1);
-                break;
-            }
-        };
+          map.removeLayer(markers[i]);
+          markers.splice(i, 1);
+          break;
+      }
+  };
 }
 
 function show_accomodation(){
   var accomodation = accomodations[$(this).attr('no')];
   var no = $(this).attr('no');
+  HotelSelecionado = no;
   var lat = accomodation.geoData.latitude;
   var lon = accomodation.geoData.longitude;
   var url = accomodation.basicData.web;
@@ -184,7 +305,7 @@ function show_accomodation(){
 
     if (!markerexists){
       markers.push(marker);
-      marker.addTo(map).bindPopup('<a no="'+ no +'" href="' + url + '">' + name + '</a><br/>' + "<a href='#desc'>+ info</a><button onclick='del("+lat+","+lon+")'>Eliminar</button>")
+      marker.addTo(map).bindPopup('<a no="'+ no +'" href="' + url + '">' + name + '</a><br/>' + "<a href='#desc'>+ info</a><button  class='del' onclick='del("+lat+","+lon+")'>Eliminar</button>")
 
     }
     marker.openPopup();
@@ -194,8 +315,8 @@ function show_accomodation(){
 };
 
 function get_accomodations(){
+  $("#get").hide();
   $.getJSON("alojamientos.json", function(data) {
-    $('#get').html('');
     accomodations = data.serviceList.service
     var list = '<p>(click on any of them for details and location in the map)</p>'
     list = list + '<div class="scroll "><ul id="sortable">'
@@ -247,10 +368,7 @@ $(document).ready(function() {
   $("#load").click(load);
   $("#save").click(save);
   $("#tabs").tabs();
-
-
   map.on("popupopen",function(){
-
     cargar_info($(this._popup._content).attr('no'))
   })
 
